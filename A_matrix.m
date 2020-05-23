3214c5591ae76212e6315827eebe7d626c6409ce
Vihaan Akshaay <akswins001@gmail.com>
2020-05-23 11:53:17 +0530

    started defining base functions

---
+++ A_matrix.m
@@ -0,0 +1,8 @@
+function A = A_matrix(phi)
+% This function returns the orthonormal rotational matrix
+
+
+A=[cos(phi),-sin(phi);sin(phi),cos(phi)]
+
+end
+
---
+++ s_rot.m
@@ -0,0 +1,6 @@
+function s_r = s_rot(s)
+
+s_r = [-s(2) ; s(1)];
+
+end
+
